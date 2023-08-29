9fb7c7644dabf8463819077f5d707f7f10dd532b
tayleredelen <taylerledelen@gmail.com>
2023-08-29 12:29:39 -0500

    Finished Flipping Bits challenge

--- FlippingBits/FlippingBits.java
+++ FlippingBits/FlippingBits.java
@@ -1,4 +1,29 @@
 package FlippingBits;
 
 public class FlippingBits {
+
+    public static long flippingBits(long n) {
+        // Convert the number to its binary representation
+        String binaryNum = Long.toBinaryString(n);
+
+        // Pad zeros to the left to make it 32 bits long
+        while (binaryNum.length() < 32) {
+            binaryNum = "0" + binaryNum;
+        }
+
+        // Create a StringBuilder to store the flipped binary representation
+        StringBuilder flippedBinary = new StringBuilder();
+
+        // Iterate through each bit and flip it using XOR (^) with 1
+        for (char bit : binaryNum.toCharArray()) {
+            flippedBinary.append((bit == '0') ? '1' : '0');
+        }
+
+        // Convert the flipped binary representation back to a long integer
+        long flippedInteger = Long.parseLong(flippedBinary.toString(), 2);
+
+        return flippedInteger;
+    }
+
+
 } 
